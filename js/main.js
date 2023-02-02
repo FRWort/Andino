3674a648e4a1eb79fa0fcdd1bb434075e094c895
Francisco Wortman <wort-fr@protonmail.ch>
2023-02-02 11:06:48 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -34,6 +34,40 @@ $(".menu").click(function(){
 });
 // ---------------------------------------------------------
 
+(function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      autoplay: true,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
+
 /*
 // Fixed with https://bobbyhadz.com/blog/javascript-getboundingclientrect-is-not-a-function
 function isElementVisible(el) {
